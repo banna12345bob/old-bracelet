536a35f79820eeda8633f29308c5018fdf4d4325
banna12345bob <banna12345bob@outlook.com>
2024-09-02 20:35:29 +1000

    Added geckolib

--- src/client/java/com/idiotss/isaac/events/KeyInputHandler.java
+++ src/client/java/com/idiotss/isaac/events/KeyInputHandler.java
@@ -2,15 +2,10 @@ package com.idiotss.isaac.events;
 
 import com.idiotss.isaac.animation.AnimatablePlayer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
 import net.minecraft.util.math.Vec3d;
-import org.lwjgl.glfw.GLFW;
 
 public class KeyInputHandler {
-//    private static KeyBinding rollKeybind;
     private static final float rollMultiplier = 2.5F;
     private static final float backsetpMultiplier = 0.5F;
     private static final float rollCooldown = 7.5F;
@@ -37,7 +32,7 @@ public class KeyInputHandler {
                 } else {
                     if (rollCooldown < client.world.getTime() - lastTime) {
                         newVelocity = new Vec3d(client.player.getRotationVector().x * rollMultiplier, client.player.getVelocity().y, client.player.getRotationVector().z * rollMultiplier);
-                        ((AnimatablePlayer) client.player).playAnimation("old-bracelet:roll", client.player.getVelocity(), 1.0F);
+                        ((AnimatablePlayer) client.player).playAnimation("old-bracelet:player/run.animation", client.player.getVelocity(), 1.0F);
                         lastTime = client.world.getTime();
                         client.player.setVelocity(newVelocity);
                     }
@@ -48,13 +43,6 @@ public class KeyInputHandler {
     }
 
     public static void register() {
-//        rollKeybind = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-//                "key.oldbracelet.rollkey", // The translation key of the keybinding's name
-//                InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
-//                GLFW.GLFW_KEY_R, // The keycode of the key
-//                KeyBinding.MOVEMENT_CATEGORY // The translation key of the keybinding's category.
-//        ));
-
         registerKeyInputs();
     }
 
